25241d6361095e5349b8846a0d6517567a315fd6
Ulrik McArdle <umc@praxis.dk>
2017-08-24 11:48:37 +0200

    Added new version to the version.php

--- version.php
+++ version.php
@@ -25,8 +25,8 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version   = 2015073102;
+$plugin->version   = 2017082400;
 $plugin->component = 'block_my_enrolled_courses';
 $plugin->requires  = 2013051400;
-$plugin->release = '2.4.1 (Build: 2015073100)';
+$plugin->release = '2.5.0 (Build: 2017082400)';
 $plugin->maturity = MATURITY_STABLE;
